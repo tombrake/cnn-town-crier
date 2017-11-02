c7ec847cdd4083eb110abec6a3ce6ae11d9c67ca
Ian Patton <ian.patton@gmail.com>
2017-11-02 16:26:32 -0400

    add branding property

--- tasks/retrieve-content.js
+++ tasks/retrieve-content.js
@@ -88,6 +88,7 @@ Promise.all([amqp.start(), sns.start(), messenger.start()])
                             event: {
                                 source: doc.dataSource,
                                 section: doc.section,
+                                branding: doc.branding,
                                 record
                             }
                         });
