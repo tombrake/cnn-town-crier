445ba21c490532e061d69f2f7d8843c82726a5d9
Tom <tom@tombrake.com>
2017-11-07 11:24:12 -0500

    Update retrieve-content.js
    schema version bump

--- tasks/retrieve-content.js
+++ tasks/retrieve-content.js
@@ -52,7 +52,7 @@ Promise.all([amqp.start(), sns.start(), messenger.start()])
                 const record = {
                     branding: doc.branding || ((doc.attributes) ? doc.attributes.branding : ''),
                     contentType: doc.type,
-                    schemaVersion: '2.1.0',
+                    schemaVersion: '2.1.1',
                     slug: doc.slug,
                     section: doc.section,
                     sourceId: doc.sourceId,
